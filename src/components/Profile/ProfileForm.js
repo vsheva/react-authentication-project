5c342d2836dd56445faf619d1022e5438a96190d
vsheva <valera.sheva@gmail.com>
2022-08-04 20:42:12 +0300

    Use token for requests to protected resourses

--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,11 +1,34 @@
 import classes from './ProfileForm.module.css';
+import {useContext, useRef} from "react";
+import AuthContext from "../../store/auth-context";
 
 const ProfileForm = () => {
+
+    const newPasswordInputRef = useRef();
+    const authCtx = useContext(AuthContext);
+
+    const submitHandler =(event)=>{
+        event.preventDefault();
+        const enteredNewPassword =  newPasswordInputRef.current.value;
+
+        fetch("https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyBI82PbewgkJW5SBIDgWtvredDKE-hXGLo", {
+          method: 'POST',
+            body: JSON.stringify({
+                idToken:authCtx.token,
+                password: enteredNewPassword,
+                returnSecureToken: false,
+            }),
+            headers: { 'Content-Type':"application/json"},
+        }).then((res)=> {
+            //assumption: Always succeeds!
+        });
+    };
+
   return (
-    <form className={classes.form}>
+    <form onSubmit={submitHandler} className={classes.form}>
       <div className={classes.control}>
         <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <input type='password' minLength="7" id='new-password' ref={newPasswordInputRef} />
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
